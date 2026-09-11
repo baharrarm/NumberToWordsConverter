9abac6be063464bb926b9d821ce4643e13d1f0f8
Bahar Arman <Emma.symlone@gmail.com>
2026-09-11 13:03:19 +1000

    Implemented the number to word convert algorithm

---
+++ Server/NumberToWordsConverter.Application/Constants/NumberWords.cs
@@ -0,0 +1,48 @@
+namespace NumberToWordsConverter.Application.Constants
+{
+    public static class NumberWords
+    {
+        public static readonly string[] UnitsMap= {
+                                        "One",
+                                        "Two",
+                                        "Three",
+                                        "Four",
+                                        "Five",
+                                        "Six",
+                                        "Seven",
+                                        "Eight",
+                                        "Nine",
+                                        "Ten",
+                                        "Eleven",
+                                        "Twelve",
+                                        "Thirteen",
+                                        "Fourteen",
+                                        "Fifteen",
+                                        "Sixteen",
+                                        "Seventeen",
+                                        "Eighteen",
+                                        "Nineteen"};
+                                        
+
+        public static readonly string[] TensMap = { 
+                                        "Ten", 
+                                        "Twenty", 
+                                        "Thirty", 
+                                        "Forty", 
+                                        "Fifty", 
+                                        "Sixty", 
+                                        "Seventy", 
+                                        "Eighty", 
+                                        "Ninety"};
+
+        public static readonly string[] Notations = {
+                                        "Hundred",
+                                        "Thousand",
+                                        "Million",
+                                        "Billion",
+                                        "Trillion",
+                                        "Quadrillion",
+                                        "Quintillion"
+                                        };
+    }
+}
--- Server/NumberToWordsConverter.Application/Services/NumberConverterService.cs
+++ Server/NumberToWordsConverter.Application/Services/NumberConverterService.cs
@@ -1,3 +1,4 @@
+using NumberToWordsConverter.Application.Constants;
 using NumberToWordsConverter.Application.Models;
 using NumberToWordsConverter.Application.Services.Interfaces;
 using NumberToWordsConverter.Application.Validators;
@@ -7,6 +8,8 @@ namespace NumberToWordsConverter.Application.Services
     public class NumberConverterService : INumberConverterService
     {
         private readonly NumberValidator _validator = new NumberValidator();
+        private readonly string[] currencyUnits = {"Dollar" , "Cent"}; 
+
         /// <summary>
         ///     Gets a decimal number and converts it into words with currency details and combines them into a complete result in Uppercase. 
         ///     Returns a failed result with error message if validation fails. 
@@ -20,7 +23,26 @@ namespace NumberToWordsConverter.Application.Services
                 return new ConversionResult(false, "", error);
             }
 
-            return new ConversionResult(true, "NumberWordsCompleteString", "");
+
+            List<string> words = new List<string>();
+            
+            var integerPart = Convert.ToUInt64(Math.Truncate(number));
+            var fractionPart = Convert.ToUInt64((number - integerPart) * 100);
+            UInt64[] numberParts = {integerPart, fractionPart};
+
+            for (int i = 0; i < numberParts.Length; i++)
+            {
+                if (numberParts[i] > 0)
+                {
+                    words.AddRange(GenerateNumberWords(numberParts[i]));
+                    words.Add(currencyUnits[i] + (numberParts[i] > 1 ? 's' : string.Empty));
+                    if (i == 0 && numberParts[i+1] > 0) words.Add("and");
+                }
+            }
+            
+            var result = String.Join(" ", words).ToUpper();
+
+            return new ConversionResult(true, result, "");
         }
 
         /// <summary>
@@ -28,23 +50,77 @@ namespace NumberToWordsConverter.Application.Services
         /// </summary>
         private List<string> GenerateNumberWords(UInt64 number)
         {
-            return [];
+            List<int> splittedNumber = SplitByNotations(number);
+            List<string> words = new List<string>();
+            
+            bool needsAnd;
+            for (int i = 0; i < splittedNumber.Count(); i++)
+            {
+                if(splittedNumber[i] != 0)
+                {
+                    needsAnd = false;
+                    if (i == 0 && splittedNumber[i] < 100 && splittedNumber.Count > 1)
+                    {
+                        needsAnd = true;
+                    }
+                    words.InsertRange(0, Map3DigitNumberToWords(splittedNumber[i], i, needsAnd));   
+                }
+
+            }
+            return words;
         }
 
         /// <summary>
-        ///     Gets the integer part of the main number and splits the number into 3-digits parts
+        ///     Gets each part of the main number and splits the number into 3-digits parts
         /// </summary>
         private List<int> SplitByNotations(UInt64 number)
         {
-            return [];
+            List<int> splittedNumber = new List<int>();
+            do
+            {
+                splittedNumber.Add((int)(number % 1000));
+                number /= 1000;
+
+            } while (number > 0);
+            return splittedNumber;
         }
 
         /// <summary>
         ///     Gets a 3-digit part of the integer part of the main number and converts it to words with notations included
         /// </summary>
-        private List<string> Map3DigitNumberToWords(int number)
+        private List<string> Map3DigitNumberToWords(int number, int notationIndex, bool needsAnd)
         {
-            return [];
+            var threeDigitNumberWord = new List<string>();
+            
+            if (number/100 > 0)
+            {
+                threeDigitNumberWord.Add(NumberWords.UnitsMap[(number / 100) - 1]);
+                threeDigitNumberWord.Add(NumberWords.Notations[0]);
+                needsAnd = true;
+                number %= 100;
+            }
+
+            if (number > 0)
+            {
+                if (needsAnd) threeDigitNumberWord.Add("And");
+                if (number < 20)
+                {
+                    threeDigitNumberWord.Add(NumberWords.UnitsMap[number - 1]);
+                }
+                else
+                {
+                    string last2Digit =  NumberWords.TensMap[(number / 10)-1];
+                    if ((number % 10) > 0)
+                    {
+                        last2Digit += "-" + NumberWords.UnitsMap[number % 10 - 1];
+                    }
+                    threeDigitNumberWord.Add(last2Digit);
+                }
+
+            }
+
+            if(notationIndex>0) threeDigitNumberWord.Add(NumberWords.Notations[notationIndex]);
+            return threeDigitNumberWord;
         }
 
         
